6e91e51484fd2f4361aece335c62c32bba8654ac
pham tri <minhtri.pham411@gmail.com>
2025-11-22 11:07:40 +0700

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive!", 200
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    Thread(target=run).start()
